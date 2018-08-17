eab43ff13ca1dd63c63b0d8228fe46d53add9784
Lucas Deane <ladeane00@yahoo.com>
2018-08-16 20:57:52 -0500

    Initial Commit

---
+++ Food Tracker 2Tests/Food_Tracker_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Food_Tracker_2Tests.swift
+//  Food Tracker 2Tests
+//
+//  Created by Lucas Deane on 8/16/18.
+//  Copyright © 2018 Lucas Deane. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Tracker_2
+
+class Food_Tracker_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
